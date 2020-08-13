23dd494e5e690bffcebef711b9e28beb68505023
zamin60 <64024649+zamin60@users.noreply.github.com>
2020-08-13 11:30:03 +0530

    Update keys.js
    keys removed

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    JWT_SECRET:"qwertyuiopasdfghjkl",
-    MONGOURI:"mongodb+srv://zamin_db:CIkHuN9xBOtydcLS@cluster-insta.2weoj.mongodb.net/test?retryWrites=true&w=majority"
+    JWT_SECRET:please provide,
+    MONGOURI: please provide
 }
